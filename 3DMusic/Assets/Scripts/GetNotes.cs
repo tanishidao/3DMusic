f66972c3d7cf19d063e4add2764de05a23ae883c
tanishidao <tanitaniumauma1003@gmail.com>
2021-11-08 05:00:07 +0900

    I can do this. 1.character's moving. 2.3types weapon. 3.Timing generate.

--- 3DMusic/Assets/Scripts/GetNotes.cs
+++ 3DMusic/Assets/Scripts/GetNotes.cs
@@ -2,22 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GetNotes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool Destroyed;
+    public bool Hit;
+    public GameObject HitImage;
+
+    private float Destroytime;   
     void Start()
     {
-        
+        Hit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        while (Destroyed == false)
+        {
+            HitImage.SetActive(false);
+            Hit = false;
+            break;
+        }
+        while (Destroyed == true)
+        {
+            Destroytime += Time.deltaTime;
+            Hit = true;
+            HitImage.SetActive(true);
+            break;
+        }
+        if (Destroyed == true && 0.1f < Destroytime)
+        {
+
+            Destroyed = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
+        Destroyed = true;
+
     }
 }
